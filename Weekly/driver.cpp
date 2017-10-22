f73516a89616ec301cafb00788dcec10dcd6aabe
Michael137 <michaelbuch12@gmail.com>
2017-10-22 17:33:00 +0100

    generated driver

--- Weekly/driver.cpp
+++ Weekly/driver.cpp
@@ -1,8 +1,13 @@
 /*
-  https://www.reddit.com/r/dailyprogrammer/comments/4vrb8n/weekly_25_escape_the_trolls/
+** URL: https://www.reddit.com/r/dailyprogrammer/comments/4vrb8n/weekly_25_escape_the_trolls/
+** Name:
+** Run Instructions:
+** Description:
+** Input:
+** Output:
 */
 
 int main(int argc, char const *argv[]) {
-  /* code */
+
   return 0;
 }
