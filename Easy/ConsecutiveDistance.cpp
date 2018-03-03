fe182aa38248847d59a937372c0525ce19a9b10d
Michael137 <michaelbuch12@gmail.com>
2018-03-03 22:04:54 +0000

    Clang-format

--- Easy/ConsecutiveDistance.cpp
+++ Easy/ConsecutiveDistance.cpp
@@ -1,42 +1,43 @@
 //https://www.reddit.com/r/dailyprogrammer/comments/759fha/20171009_challenge_335_easy_consecutive_distance/
 
-#include <iostream>
 #include <algorithm>
-#include <vector>
-#include <iterator>
 #include <cmath>
+#include <iostream>
+#include <iterator>
+#include <vector>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[])
+{
 
-  int seqs = 0,
-      seqs_len = 0;
+    int seqs = 0,
+        seqs_len = 0;
 
-  std::cin >> seqs >> seqs_len;
+    std::cin >> seqs >> seqs_len;
 
-  for(int i = 0; i < seqs; i++) {
-    //Lazy copying of input into array...
-    std::vector<int> distances;
-    std::vector<std::pair<int,int>> dist_pairs;
-    copy_n(std::istream_iterator<int>(std::cin), seqs_len, std::back_inserter(distances));
+    for (int i = 0; i < seqs; i++) {
+        //Lazy copying of input into array...
+        std::vector<int> distances;
+        std::vector<std::pair<int, int>> dist_pairs;
+        copy_n(std::istream_iterator<int>(std::cin), seqs_len, std::back_inserter(distances));
 
-    for(int j = 0; j < distances.size(); j++) {
-      dist_pairs.push_back({distances[j], j});
+        for (int j = 0; j < distances.size(); j++) {
+            dist_pairs.push_back({ distances[j], j });
+        }
+
+        struct {
+            bool operator()(const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) const { return lhs.first < rhs.first; }
+        } cmp;
+        std::sort(dist_pairs.begin(), dist_pairs.end(), cmp);
+
+        double sum = 0;
+        for (int k = 0; k < dist_pairs.size() - 1; k++) {
+            if (dist_pairs[k].first - dist_pairs[k + 1].first == -1) {
+                sum += std::abs(dist_pairs[k].second - dist_pairs[k + 1].second);
+            }
+        }
+
+        std::cout << sum << std::endl;
     }
 
-    struct {
-      bool operator()(const std::pair<int,int>& lhs, const std::pair<int,int>& rhs) const { return lhs.first < rhs.first; }
-    } cmp;
-    std::sort(dist_pairs.begin(), dist_pairs.end(), cmp);
-
-    double sum = 0;
-    for(int k = 0; k < dist_pairs.size()-1; k++) {
-      if(dist_pairs[k].first - dist_pairs[k+1].first == -1) {
-        sum+=std::abs(dist_pairs[k].second - dist_pairs[k+1].second);
-      }
-    }
-
-    std::cout << sum << std::endl;
-  }
-
-  return 0;
+    return 0;
 }
--- Easy/TalkingClock.cpp
+++ Easy/TalkingClock.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
-#include<unordered_map>
-#include<sstream>
-#include<string>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
-static const unordered_map<int, string> digitsMap =
-{
+static const unordered_map<int, string> digitsMap = {
     { 1, "one" },
     { 2, "two" },
     { 3, "three" },
@@ -31,50 +30,48 @@ static const unordered_map<int, string> digitsMap =
     { 50, "fifty" }
 };
 
-int convert( const int h )
+int convert(const int h)
 {
-    if( h == 0 )
+    if (h == 0)
         return 12;
 
-    return ( h > 12 ) ? h - 12 : h;
+    return (h > 12) ? h - 12 : h;
 }
 
-string hrsToString( const int h )
+string hrsToString(const int h)
 {
-    return digitsMap.find( convert( h ) )->second;
+    return digitsMap.find(convert(h))->second;
 }
 
-int firstDigit( int n )
+int firstDigit(int n)
 {
-    while( n >= 10 )
+    while (n >= 10)
         n /= 10;
-    
+
     return n;
 }
 
-string minToString( const int m )
+string minToString(const int m)
 {
-    ostringstream oss( "" );
-    
-    if( m != 0 )
-    {
-        if( m < 10 )
+    ostringstream oss("");
+
+    if (m != 0) {
+        if (m < 10)
             oss << "oh ";
 
-        if( m < 20 )
-            oss << digitsMap.find( m )->second;
-        else
-        {
-            int first = firstDigit( m ) * 10;
-            oss << digitsMap.find( first )->second << " ";
-            oss << digitsMap.find( m - first )->second;
+        if (m < 20)
+            oss << digitsMap.find(m)->second;
+        else {
+            int first = firstDigit(m) * 10;
+            oss << digitsMap.find(first)->second << " ";
+            oss << digitsMap.find(m - first)->second;
         }
     }
 
     return oss.str();
 }
 
-bool isPm( const int h )
+bool isPm(const int h)
 {
     return h >= 12;
 }
@@ -82,13 +79,13 @@ bool isPm( const int h )
 int main()
 {
     int h, m;
-    
+
     cin >> h;
     cin.ignore();
     cin >> m;
-    
+
     ostringstream oss("It's ");
-    oss << hrsToString( h ) << " ";
-    oss << minToString( m ) << " " << ( isPm( h ) ? "pm" : "am" );
+    oss << hrsToString(h) << " ";
+    oss << minToString(m) << " " << (isPm(h) ? "pm" : "am");
     cout << oss.str() << endl;
 }
