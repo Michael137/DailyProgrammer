ce5cc3ee6839ba02589e1e369e3176aba3072b94
Michael Buch <michaelbuch12@gmail.com>
2018-08-30 22:12:28 +0200

    sales

---
+++ Intermediate/SalesCommissions.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* Input */
+/*
+** Rvenue
+** 
+**             Johnver Vanston Danbree Vansey  Mundyke
+** Tea             190     140    1926     14      143
+** Coffee          325      19     293   1491      162
+** Water           682      14     852     56      659
+** Milk            829     140     609    120       87
+** 
+** Expenses
+** 
+**             Johnver Vanston Danbree Vansey  Mundyke
+** Tea             120      65     890     54      430
+** Coffee          300      10      23    802      235
+** Water            50     299    1290     12      145
+** Milk             67     254      89    129       76
+*/
+
+/* Output */
+/*
+**                 Fank   Jane
+** Commission       6.20   9.49
+*/
+
+int main() {
+	// parse
+	// build table of objects
+	// calculate profits
+	// calculate commissions (6.2% of non-negative profits)
+	return 0;
+}
