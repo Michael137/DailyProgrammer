5f2e8d79b237fdbad7f54bb05f574f6f9de148a3
Michael Buch <michaelbuch12@gmail.com>
2018-09-03 12:11:05 +0200

    word funnel with trie
    Former-commit-id: 6dcc9d7037c4c93f165de2aba848b9c882f55f36

--- Easy/WordFunnel.c
+++ Easy/WordFunnel.c
@@ -33,20 +33,50 @@ void read_input_prompt( char** str ) { read_input( str, stdin ); }
 
 void read_input_file( char** str, FILE* fp ) { read_input( str, fp ); }
 
+void build_trie( Trie** trie, FILE* fp )
+{
+	char* in_str;
+	t_create( trie );
+
+	while( !feof( fp ) ) {
+		read_input_file( &in_str, fp );
+		t_insert( trie, in_str );
+		free( in_str );
+	}
+
+	rewind( fp );
+}
+
 bool check_strings( char* str1, char const* str2 )
 {
 	uint64_t const len = strlen( str1 );
 	for( uint64_t i = 0; i < len; ++i ) {
-
 		// Remove character from string
-		char tmp[len];
-		strncpy( tmp, str1, len );
+		char tmp[len + 1];
+		strncpy( tmp, str1, sizeof( tmp ) );
 		memmove( &tmp[i], &tmp[i + 1], len - i );
 
 		if( strcmp( tmp, str2 ) == 0 ) return true;
 	}
 
 	return false;
+}
+
+int funnel_trie( Trie const* trie, char* str )
+{
+	int count = 0;
+	uint64_t const len = strlen( str );
+	for( uint64_t i = 0; i < len; ++i ) {
+		// Remove character from string
+		char tmp[len + 1];
+		strncpy( tmp, str, sizeof( tmp ) );
+		memmove( &tmp[i], &tmp[i + 1], len - i );
+
+		if( t_search( trie, tmp ) && !( ( i > 0 && str[i] == str[i - 1] ) ) )
+			count++;
+	}
+
+	return count;
 }
 
 void challenge()
@@ -66,56 +96,41 @@ void challenge()
 	fin_str = NULL;
 }
 
-void bonus()
-{
-	char *mut_str, *fin_str;
-	FILE* fp;
-	fp = fopen( "enable1.txt", "r" );
-
-	read_input_prompt( &mut_str );
-
-	while( !feof( fp ) ) {
-		read_input_file( &fin_str, fp );
-		if( check_strings( mut_str, fin_str ) ) printf( "%s\n", fin_str );
-		free( fin_str );
-	}
-
-	fclose( fp );
-
-	free( mut_str );
-	mut_str = NULL;
-	fin_str = NULL;
-}
-
 // Find all words in enable1.txt that yield
 // check_strings( word, another word in enable1.txt ) for five
 // words in the enable1.txt list
 //
 // e.g. boats
-void bonus2()
+int bonus2( bool print )
 {
-	char* in_str;
-	Trie* trie;
-	t_create( &trie );
-
 	FILE* fp;
+	int num_found = 0;
+	char* in_str;
+
 	fp = fopen( "enable1.txt", "r" );
+
+	Trie* trie;
+	build_trie( &trie, fp );
 
 	while( !feof( fp ) ) {
 		read_input_file( &in_str, fp );
-		t_insert( &trie, in_str );
+		if( funnel_trie( trie, in_str ) == 5 ) {
+			if( print ) printf( "%s\n", in_str );
+			num_found++;
+		}
 		free( in_str );
 	}
 
 	t_free( trie );
 	fclose( fp );
+
+	return num_found;
 }
 
 int main()
 {
-	// challenge();
-	// bonus();
-	bonus2();
+//	challenge();
+	printf( "Found %d words funnelling 5 other words.\n", bonus2( false ) );
 
 	return 0;
 }
